a1c59409b9c5d773526f912ceb486e6da71d9ff3
Yasinmomin <yasinmomin171@gmail.com>
2026-07-13 12:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
- const toggle = document.getElementById("navtoggle");
- const menu = document.getElementById("navmenu");
 
- toggle.addEventListener('click', ()  => {
-
-    const isopen = menu.classList.toggle('open');
-    toggle.setAttribute('aria-expanded',isopen);
- });
-
- menu.querySelectorAll('a').forEach(link =>{
-    link.addEventListener('click',() =>{
-        menu.classList.remove('open');
-        toggle.setAttribute('aria-expanded','false');
-
-    })
- });
  const canvas = document.getElementById('frameCanvas')
 const context = canvas.getContext('2d')
 
